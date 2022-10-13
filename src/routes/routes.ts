2d1bbf4e8767897fc944df42eac032803d8c892c
Ayinla Abeeb <abeebayinla@gmail.com>
2022-10-13 02:55:01 +0100

    added contact us route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { Express, Request, Response, NextFunction } from 'express';
 import IndexRoute from './index.routes';
 import HealthcheckRoute from './health.routes';
 import UserAuthRoute from './user/auth.routes';
+import ContactRoute from './contact.routes';
 // import RoleRoute from './role.routes';
 
 export default function (app: Express) {
@@ -12,12 +13,11 @@ export default function (app: Express) {
         );
         next();
     });
+
     // general routes
     app.use('/', IndexRoute);
     app.use('/health_check', HealthcheckRoute);
-
-    // role routes
-    // app.use('/api/roles', RoleRoute);
+    app.use('/api/contact_us', ContactRoute);
 
     // user routes
     app.use('/api/users/auth', UserAuthRoute);
