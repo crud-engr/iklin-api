287bcd2deb3cdbe9a192b58f907cd971289d44db
Ayinla Abeeb <abeebayinla@gmail.com>
2023-02-26 23:28:41 +0100

    remove user card and add login policy

--- routes/user/auth.routes.ts
+++ routes/user/auth.routes.ts
@@ -28,10 +28,6 @@ router
 router.route('/save_location').post(new AuthController().saveLocation);
 
 router
-    .route('/save_card')
-    .post([new AuthPolicy().validateSaveCard], new AuthController().saveCard);
-
-router
     .route('/resend_token')
     .post(
         [new AuthPolicy().validateBasicRegistration],
@@ -47,6 +43,8 @@ router
 
 router.route('/forgot_password').post(new AuthController().forgotPassword);
 
-router.route('/login').post(new AuthController().login);
+router
+    .route('/login')
+    .post([new AuthPolicy().validateLogin], new AuthController().login);
 
 export default router;
